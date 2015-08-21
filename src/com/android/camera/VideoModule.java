7b37a82f1fc05d642cc13007d33896a3474475a8
Sultanxda <sultanxda@gmail.com>
2015-08-20 18:43:29 -0700

    SnapdragonCamera: Hide UI after error-checking video preferences
    * Hiding the UI before error-checking the video preferences when
      video recording starts results in a broken UI when one of the
      error cases evaluates to be true.
    * Hide the UI after the error checking to prevent the UI from
      breaking when an error is found.
    
    Change-Id: Ia98798dbf2a33800d47497630714af17865767c6

--- src/com/android/camera/VideoModule.java
+++ src/com/android/camera/VideoModule.java
@@ -1832,7 +1832,6 @@ public class VideoModule implements CameraModule,
         mStartRecPending = true;
         mUI.cancelAnimations();
         mUI.setSwipingEnabled(false);
-        mUI.hideUIwhileRecording();
         // When recording request is sent before starting preview, onPreviewFrame()
         // callback doesn't happen so removing preview cover here, instead.
         if (mUI.isPreviewCoverVisible()) {
@@ -1904,6 +1903,8 @@ public class VideoModule implements CameraModule,
             mStartRecPending = false;
             return false;
         }
+
+        mUI.hideUIwhileRecording();
 
         // Make sure the video recording has started before announcing
         // this in accessibility.
